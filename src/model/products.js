f7bf692f28e67a94391a7ed89b263ebd4db6ad4e
halimahexe <127022084+halimahexe@users.noreply.github.com>
2024-06-09 16:41:21 +0100

    completing challenge 2

--- src/model/products.js
+++ src/model/products.js
@@ -1,5 +1,7 @@
 const db = require('../database/db.js');
 
+// List all products
+
 const select_products = db.prepare(/* sql */ `
     SELECT id, name, quantity_per_unit, unit_price, units_in_stock, units_on_order FROM products
 `);
@@ -8,4 +10,16 @@ const listProducts = () => {
 	return select_products.all();
 };
 
-module.exports = { listProducts };
+// Search products
+
+const search_products = db.prepare(/* sql */ `
+    SELECT id, name
+    FROM products
+    WHERE name LIKE ?
+`);
+
+const searchProducts = (string) => {
+	return search_products.all('%' + string + '%');
+};
+
+module.exports = { listProducts, searchProducts };
